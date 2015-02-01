55d454a3145e4ca381193ae2016739ed070abaec
Renan Iglesias <renan.iglesias@ufrj.br>
2015-01-31 22:14:59 -0200

    Decoder

--- src/br/ufrj/ppgi/huffmanmapreduce/Main.java
+++ src/br/ufrj/ppgi/huffmanmapreduce/Main.java
@@ -15,11 +15,11 @@ public class Main {
 		in = new String(args[0]);
 		out = new String(in);
 		cb = new String(in);
-		out += ".dir/compressed";
-		cb += ".dir/codification";
+		out += ".mapreducedir/compressed";
+		cb += ".mapreducedir/codification";
 
 		try {
-			fs.delete(new Path(args[0] + ".dir"), true);
+			fs.delete(new Path(args[0] + ".mapreducedir"), true);
 		} 
 		catch(Exception ex) {
 			
@@ -31,11 +31,11 @@ public class Main {
 		t = t2 - t1;
 		System.out.println(t/1000000000.0 + " s (encoder)");
 
-		//in += ".dir/decompressed";
-		//t1 = System.nanoTime();
-		//new Decoder(out, in, cb);
-		//t2 = System.nanoTime();
-		//t = t2 - t1;
-		//System.out.println(t/1000000000.0 + " s (decoder)");
+		in += ".mapreducedir/decompressed";
+		t1 = System.nanoTime();
+		new Decoder(out, in, cb);
+		t2 = System.nanoTime();
+		t = t2 - t1;
+		System.out.println(t/1000000000.0 + " s (decoder)");
 	}
 }
