fc0686ab0505a302f1333c8c903207dea41a6e44
Renan Iglesias <renan.iglesias@ufrj.br>
2015-02-08 16:59:46 -0200

    Full encoder

--- src/br/ufrj/ppgi/huffmanmapreduce/mapreduce/symbolcount/SymbolCountConfiguration.java
+++ src/br/ufrj/ppgi/huffmanmapreduce/mapreduce/symbolcount/SymbolCountConfiguration.java
@@ -18,9 +18,6 @@ public class SymbolCountConfiguration extends Configured implements Tool {
 	public int run(String[] args) throws Exception {
 		// When implementing tool
 		Configuration conf = this.getConf();
-		//args[0] = conf.get("fs.defaultFS").concat(args[0]);
-		
-		conf.set("yarn.app.mapreduce.am.log.level", "DEBUG");
 		
 		// Create job
 		Job job = Job.getInstance(conf, "huffmanSymbolCount");
@@ -41,7 +38,7 @@ public class SymbolCountConfiguration extends Configured implements Tool {
 		job.setMapOutputValueClass(LongWritable.class);
 	
 		// Output
-		FileOutputFormat.setOutputPath(job, new Path(args[0] + ".dir/symbolcount"));
+		FileOutputFormat.setOutputPath(job, new Path(args[0] + ".mapreducedir/symbolcount"));
 		job.setOutputFormatClass(ByteCountOutputFormat.class);
 		
 		// Execute job and return status (false -> don't show messages)
