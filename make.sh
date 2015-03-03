601b94a2d08e9e3c22fd076de8c6655b62f3b634
Renan Iglesias <renan.iglesias@ufrj.br>
2015-03-03 01:23:31 -0300

    Testing new version

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir bin
-mkdir release
+mkdir -p bin
+mkdir -p release
 
 javac src/br/ufrj/ppgi/huffmanmapreduce/*.java src/br/ufrj/ppgi/huffmanmapreduce/mapreduce/encoder/*.java src/br/ufrj/ppgi/huffmanmapreduce/mapreduce/io/*.java src/br/ufrj/ppgi/huffmanmapreduce/mapreduce/symbolcount/*.java src/br/ufrj/ppgi/huffmanmapreduce/mapreduce/decoder/*.java -d bin
 
